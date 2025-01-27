967b2bfe09c00f56c9770dd6b7165f5676f3a5b8
Anton Iosifov <iosifov.a.14@gmail.com>
2025-01-28 00:28:01 +0200

    prev

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+console.log("JavaScript connected!");
 
 /* ------------------ AOS library ------------------ */
 AOS.init({
@@ -21,54 +22,48 @@ document.querySelector('.logo').addEventListener('click', function (e) {
     }
 });
 
+document.querySelectorAll('.nav-list-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetId = this.getAttribute('href').substring(1); 
+        if (['about', 'skills', 'projects'].includes(targetId)) 
+        { 
+            e.preventDefault(); // Отменить стандартное поведение
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) 
+            {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
+});
 
-// document.querySelectorAll('.nav-list-link').forEach(link => {
-//     link.addEventListener('click', function (e) {
-//         e.preventDefault(); 
-//         const targetId = this.getAttribute('href').substring(1);
-//         const targetElement = document.getElementById(targetId);
+document.getElementById('form').addEventListener('submit', async function (e) {
+    e.preventDefault(); // Остановить стандартное поведение формы
 
-//         if (targetElement) 
-//         {
-//             targetElement.scrollIntoView({
-//                 behavior: 'smooth', 
-//                 block: 'start' // Прокручиваем до начала элемента
-//             });
-//         }
-//     });
-// });
-
-
-fetch('http://localhost:8000/php/server.php?page=index')
-        .then(response => response.json())
-        .then(data => {
-            console.log(`views: ${data.views}`);
-            document.getElementById('views-count').textContent = `views: ${data.views}`;
-        })
-        .catch(error => console.error('error:', error));
-
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Предотвращаем перезагрузку страницы
-
-    const form = event.target;
+    const form = e.target;
     const formData = new FormData(form);
 
-    fetch('./php/server.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 'success') {
-            // Скрываем форму и показываем сообщение
-            form.style.display = "none";
-            const successMessage = document.getElementById("success-message");
-            successMessage.style.display = "block";
-            successMessage.innerHTML = `<p>${data.message}</p>`;
+    try {
+        const response = await fetch(form.action, {
+            method: form.method,
+            body: formData,
+            headers: { 'Accept': 'application/json' }
+        });
+
+        if (response.ok) {
+            // Скрыть форму и показать сообщение об успехе
+            document.getElementById('form').style.display = 'none';
+            document.getElementById('success-message').style.display = 'block';
         } else {
-            alert(data.message);
+            alert('There was an issue sending your message. Please try again later.');
         }
-    })
-    .catch(error => console.error('Error:', error));
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again later.');
+    }
 });
+
+
+
